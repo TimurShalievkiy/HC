4796252151c77a568b446bd4c55e720204036d8c
lKORENl <korenanon@gmail.com>
2019-03-02 14:26:48 +0200

    first com

---
+++ CatWord/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float helth;
+    public float damage;
+    public Slider helthBar;
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        helthBar.maxValue = helth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (helthBar.value > 0)
+        {
+            helthBar.value -= Time.deltaTime;
+        }
+        else
+            GameObject.Destroy(gameObject);
+    }
+    public void GetDamage(float dam)
+    {
+        helth -= dam;
+    }
+    public float DoDamage()
+    {
+        return damage;
+    }
+}
---
+++ CatWord/Assets/Scripts/ScenesNavigation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScenesNavigation : MonoBehaviour
+{
+    public  void GotoGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public  void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
