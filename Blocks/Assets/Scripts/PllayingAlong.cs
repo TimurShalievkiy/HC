069b992878aad6b9995df6cb8f3b0004619b72e5
lKORENl <korenanon@gmail.com>
2019-03-14 17:44:07 +0200

    olmost done revive alg

--- Blocks/Assets/Scripts/PllayingAlong.cs
+++ Blocks/Assets/Scripts/PllayingAlong.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PllayingAlong : MonoBehaviour
 {
@@ -25,57 +26,55 @@ public class PllayingAlong : MonoBehaviour
         foreach (ResultShape item in listResultShapes)
         {
             item.listResultShapes = GetListResultShapesByField(FieldCondition.CheckAndRemoveFullLines(FieldCondition.PlaceShape(field, item.shapePos)), ref counter);
-
-            //foreach (var item2 in item.listResultShapes)
-            //{
-            //    item2.listResultShapes = GetListResultShapesByField(FieldCondition.CheckAndRemoveFullLines(FieldCondition.PlaceShape(field, item2.shapePos)), ref counter);
-            //}
-
-            //int countOfAllLine = item.countOfFullLine;
-            //int max = countOfAllLine;
-
-            //ResultShape buff = new ResultShape(-1, 0, null);
-            //foreach (ResultShape item2 in item.listResultShapes)
-            //{
-
-            //    countOfAllLine += item.countOfFullLine;
-            //    countOfAllLine += item2.countOfFullLine;
-            //    if (countOfAllLine > max)
-            //    {
-            //        max = countOfAllLine;
-            //        buff = item2;
-            //    }
-            //    countOfAllLine = 0;
-            //}
-
-            //item.ShowResultShape();
-
-            //buff.ShowResultShape();
-
-            //FieldCondition.ShowField(field);
-
-            //int[,] q = FieldCondition.PlaceShape(field, item.shapePos);
-            //int w = 0;
-            //FieldCondition.ShowField(q);
-
-            //q = FieldCondition.RemoveFullLines(q, FieldCondition.CheckFieldForFullLines(q, out w));
-            //FieldCondition.ShowField(q);
-            //if (buff.shapePos != null)
-            //{
-            //Debug.Log("buff.shapePos = " + buff.shapePos);
-            //Debug.Log("buff.countOfFullLine = " + buff.countOfFullLine);
-            //    q = FieldCondition.PlaceShape(q, buff.shapePos);
-            //FieldCondition.ShowField(q);
-            //FieldCondition.ShowField(FieldCondition.RemoveFullLines(q, FieldCondition.CheckFieldForFullLines(q, out w)));
-
-           
-            //}
-            //Debug.Log("--------------------------------------------- ");
-            //Debug.Log("max = " + max);
-            //Debug.Log("--------------------------------------------- ");
         }
 
-        Debug.Log("counter = " + counter);
+        foreach (ResultShape item in listResultShapes)
+        {
+            item.GetCountOfAllFullLine();
+            item.GetShapeWithMaxBBlock();
+
+        }
+        //разобраться в получении фигур количества 
+        if (listResultShapes != null && listResultShapes.Count > 0)
+        {
+            int max = listResultShapes.Max(x => x.countOfAllFullLine);
+
+
+            List<ResultShape> listResult = listResultShapes.FindAll(x => x.countOfAllFullLine == max);
+
+            max = 0;
+
+            foreach (var item in listResult)
+            {
+                if (item.ShapeWithMaxBlock != null)
+                    if (max < item.shapePos.Count + item.ShapeWithMaxBlock.shapePos.Count)
+                    {
+                        max = item.shapePos.Count + item.ShapeWithMaxBlock.shapePos.Count;
+                    }
+            }
+
+            ResultShape shapes = null;
+
+            foreach (var item in listResult)
+            {
+                if (item.ShapeWithMaxBlock != null)
+                    if ((item.shapePos.Count + item.ShapeWithMaxBlock.shapePos.Count) == max)
+                    {
+                        shapes = item;
+                    }
+            }
+
+
+            shapes.ShowResultShape();
+
+            if (shapes.ShapeWithMaxBlock != null)
+                shapes.ShapeWithMaxBlock.ShowResultShape();
+            else
+                Debug.Log("null");
+        }
+        //добавить последней фигуру которая помещается в поле или единичный блок
+
+        //Debug.Log("counter = " + counter);
     }
 
     public List<ResultShape> GetListResultShapesByField(int[,] field, ref int counter)
@@ -120,7 +119,9 @@ public class ResultShape
     public int id;
     public List<int> shapePos;
     public int countOfFullLine;
+    public int countOfAllFullLine;
     public List<ResultShape> listResultShapes;
+    public ResultShape ShapeWithMaxBlock;
 
     public ResultShape(int id, int countOfFullLine, List<int> shapePos)
     {
@@ -131,10 +132,80 @@ public class ResultShape
 
     public void ShowResultShape()
     {
-        if(shapePos!=null)
-        Debug.Log("id = " + id + " firstSq = " + shapePos[0] + " countOfFullLine = " + countOfFullLine);
+        if (shapePos != null)
+            Debug.Log("id = " + id + " firstSq = " + shapePos.Count + " countOfFullLine = " + countOfFullLine);
     }
+
+    public void GetCountOfAllFullLine()
+    {
+        // проверить метод на правильность
+        int countOfAllLine;
+        int max = 0;
+        countOfAllLine = countOfFullLine;
+        max = countOfAllLine;
+
+        ResultShape buff = new ResultShape(-1, 0, null);
+        foreach (ResultShape item2 in listResultShapes)
+        {
+
+            countOfAllLine += item2.countOfFullLine;
+            if (countOfAllLine > max)
+            {
+                max = countOfAllLine;
+                buff = item2;
+            }
+            countOfAllLine = countOfFullLine;
+        }
+        countOfAllFullLine = max;
+    }
+
+    public void GetShapeWithMaxBBlock()
+    {
+        if (listResultShapes == null && listResultShapes.Count == 0)
+            ShapeWithMaxBlock = null;
+
+
+        int max = 0;
+        foreach (var item in listResultShapes)
+        {
+            if (item.shapePos.Count > max)
+                max = item.shapePos.Count;
+        }
+
+        ShapeWithMaxBlock = listResultShapes.Find(x => x.shapePos.Count == max);
+    }
+
 }
 
 
 
+//foreach (var item2 in item.listResultShapes)
+//{
+//    item2.listResultShapes = GetListResultShapesByField(FieldCondition.CheckAndRemoveFullLines(FieldCondition.PlaceShape(field, item2.shapePos)), ref counter);
+//}
+
+
+
+// item.ShowResultShape();
+
+// buff.ShowResultShape();
+
+//FieldCondition.ShowField(field);
+
+//int[,] q = FieldCondition.PlaceShape(field, item.shapePos);
+//int w = 0;
+//FieldCondition.ShowField(q);
+
+//q = FieldCondition.RemoveFullLines(q, FieldCondition.CheckFieldForFullLines(q, out w));
+//FieldCondition.ShowField(q);
+//if (buff.shapePos != null)
+//{
+//    q = FieldCondition.PlaceShape(q, buff.shapePos);
+//    FieldCondition.ShowField(q);
+//    FieldCondition.ShowField(FieldCondition.RemoveFullLines(q, FieldCondition.CheckFieldForFullLines(q, out w)));
+
+
+//}
+//Debug.Log("--------------------------------------------- ");
+//Debug.Log("max = " + max);
+//Debug.Log("--------------------------------------------- ");
