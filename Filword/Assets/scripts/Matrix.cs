6413bf44ed1434aa25886b3e372d5daac9faa7cc
lKORENl <korenanon@gmail.com>
2019-08-22 12:20:02 +0300

    new olgo

--- Filword/Assets/scripts/Matrix.cs
+++ Filword/Assets/scripts/Matrix.cs
@@ -2,21 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Matrix : MonoBehaviour
+public class Matrix 
 {
-    int[,] matrixArray;
-    int[,] CountsAndWordsLength;
+    public int[,] matrixArray;
+
+    List<List<int>> wordsCellsPosition;
+
     int minLength;
     int maxLength;
 
-    int[,] RoteteMatrix()
+    public Matrix(int[,] matrixArray)
     {
-        return new int[1,1];
+        this.matrixArray = matrixArray;
     }
 
-    int[,] MirrorMatrix()
+    #region operations
+    public void Func()
     {
-        return new int[1,1];
+        // RoteteMatrixLeft();
+
+        ShowMatrix(RoteteMatrixLeft90());
     }
 
+    int[,] RoteteMatrixLeft90()
+    {
+        int length1 = matrixArray.GetLength(0);
+        int length2 = matrixArray.GetLength(1);
+        int[,] buff = new int[length1, length2];
+
+        for (int i = 0; i < length1; i++)
+        {
+            for (int j = 0; j < length2; j++)
+            {               
+                buff[i, j] = matrixArray[j, length2 - 1-i];
+            }
+        }
+
+
+
+        return buff;
+    }
+
+    int[,] RoteteMatrixLeft180()
+    {
+        int length1 = matrixArray.GetLength(0);
+        int length2 = matrixArray.GetLength(1);
+
+        int[,] buff = new int[length1, length2];
+
+        for (int i = 0; i < length1; i++)
+        {
+            for (int j = 0; j < length2; j++)
+            {
+                buff[i, j] = matrixArray[length1 - i - 1, length2 - j - 1];
+            }
+        }
+
+
+
+        return new int[1, 1];
+    }
+
+    int[,] MirrorMatrixHorisontal()
+    {
+        int length1 = matrixArray.GetLength(0);
+        int length2 = matrixArray.GetLength(1);
+
+
+        int[,] buff = new int[length1, length2];
+
+
+        for (int i = 0; i < length1; i++)
+        {
+            for (int j = 0; j < length2; j++)
+            {
+                buff[i, j] = matrixArray[length1 - i - 1, j];
+            }
+        }
+
+        return buff;
+    }
+    int[,] MirrorMatrixVertical()
+    {
+        int length1 = matrixArray.GetLength(0);
+        int length2 = matrixArray.GetLength(1);
+
+
+        int[,] buff = new int[length1, length2];
+
+
+        for (int i = 0; i < length1; i++)
+        {
+            for (int j = 0; j < length2; j++)
+            {
+                buff[i, j] = matrixArray[i, length2 - j - 1];
+            }
+        }
+
+        return buff;
+    }
+    public void ShowMatrix(int[,] arr)
+    {
+        int length1 = arr.GetLength(0);
+        int length2 = arr.GetLength(1);
+        string s  = "";
+        for (int i = 0; i < length1 ; i++)
+        {
+            for (int j = 0; j < length2; j++)
+            {
+                s += arr[i, j] + " ";
+            }
+            s += "\n";
+        }
+        Debug.Log(s);
+    }
+    #endregion operations
 }
---
+++ Filword/Assets/scripts/MatrixList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MatrixList : MonoBehaviour
+{
+    
+    List<Matrix> list ;
+
+    private void Awake()
+    {
+        list = new List<Matrix>();
+
+        list.Add(new Matrix(new int[,] {
+            { 1,1,},
+            { 1,1}
+        }));
+
+        list.Add(new Matrix(new int[,] {
+            { 2,1,1},
+            { 2,2,1},
+            { 2,2,1}
+        }));
+
+        
+    }
+}
