55beb9e007a407fdc43e0a6b871012dfd808ebae
lKORENl <korenanon@gmail.com>
2019-09-19 16:01:52 +0300

    camera

--- Sky tower/Assets/Scripts/CameraController.cs
+++ Sky tower/Assets/Scripts/CameraController.cs
@@ -7,27 +7,25 @@ public class CameraController : MonoBehaviour
     [SerializeField] float distanceToBlock;
     [SerializeField] Transform Crane;
     [SerializeField] Transform home;
-    Camera camera;
+    Camera Mainamera;
     bool needToMove = false;
     public static int countOfBlock = 0;
     public static int hard = 0;
 
     private void Start()
     {
-        camera = Camera.main;
-        Crane.position = new Vector3(Crane.transform.position.x,  transform.position.y + camera.orthographicSize + 7);
+        Mainamera = Camera.main;
+        Crane.position = new Vector3(Crane.transform.position.x,  transform.position.y + Mainamera.orthographicSize + 7);
         countOfBlock = 0;
         hard = 0;
     }
     // Update is called once per frame
     void Update()
     {
-       // if (needToMove)
-       // { 
-            Crane.position =new Vector3(Crane.transform.position.x,  camera.rect.y + transform.position.y + camera.orthographicSize + 7);
-            transform.position =Vector3.Lerp(transform.position, new Vector3(0, home.position.y+ camera.orthographicSize + 3.9f* countOfBlock, -10),0.1f);
-            //transform.position += new Vector3(0, 2 * Time.deltaTime);
-       // }
+
+            Crane.position =new Vector3(Crane.transform.position.x,  Mainamera.rect.y + transform.position.y + Mainamera.orthographicSize + 7);
+            transform.position =Vector3.Lerp(transform.position, new Vector3(0, home.position.y+ Mainamera.orthographicSize + 3.9f* countOfBlock, -10),0.1f);
+
     }
     
 }
