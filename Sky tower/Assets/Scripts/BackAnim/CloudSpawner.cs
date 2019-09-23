ba3b3423bc16b890db68d1ef5564ff3c205e8906
lKORENl <korenanon@gmail.com>
2019-09-23 15:09:15 +0300

    cloud generator

---
+++ Sky tower/Assets/Scripts/BackAnim/CloudSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    Sprite[] clouds;
+    [SerializeField]float chance = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clouds = Resources.LoadAll<Sprite>("Clouds");       
+    }
+
+    private void Update()
+    {
+        if (Random.Range(0, 100) <= chance)
+        {
+            GameObject g = new GameObject();
+            g.AddComponent<SpriteRenderer>();
+            g.GetComponent<SpriteRenderer>().sprite = clouds[Random.Range(0, clouds.Length-1)];
+            g.AddComponent<MoveToLineDirection>();
+        }
+    }
+
+}
---
+++ Sky tower/Assets/Scripts/BackAnim/MoveToLineDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToLineDirection : MonoBehaviour
+{
+    [SerializeField] float speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-speed * Time.deltaTime, 0);
+    }
+}
