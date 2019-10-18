78fa52b2bb3790ad6e794977b009adf8d71c28e3
timur_shalievskiy <korenanon@gmail.com>
2019-10-18 09:37:30 +0300

    fix manifest

---
+++ obfusLuck/Assets/modules/OneSignalObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneSignalObject : MonoBehaviour
+{
+
+    void Start()
+    { 
+        OneSignal.StartInit("6f7f302e-3cf4-4c0c-ab78-57433f539b72")
+          .HandleNotificationOpened(HandleNotificationOpened)
+          .EndInit();
+
+        OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+
+        OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+    }
+
+    private static void HandleNotificationOpened(OSNotificationOpenedResult result)
+    {
+        // use notification opened result here
+    }
+}
---
+++ obfusLuck/Assets/modules/connecting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class connecting : MonoBehaviour
+{
+    [SerializeField]Text text;
+    [SerializeField] float delay;
+    float currentDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Loading";
+        currentDelay = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentDelay -= Time.deltaTime;
+        if (currentDelay <= 0)
+        {
+            if (text.text.Length > 9)
+                text.text = "Loading";
+            text.text += ".";
+            currentDelay = delay;
+        }
+    }
+}
